3afa70fb364db54515b167756d8936f1f722c71d
Winterson <u19cse1028@cit.ac.in>
2025-08-10 23:30:05 +0530

    modify: revert changes to response dto

--- src/dtos/response.dto.ts
+++ src/dtos/response.dto.ts
@@ -1,18 +1,22 @@
-import z from "zod"
+import z from "zod";
 
-const SuccessResponse = <T extends z.ZodTypeAny>(schema: T) => z.object({
-	success: z.literal(true),
-	data: schema,
-	meta: z.record(z.string(), z.any()).optional(),
-})
+const SuccessResponse = <T extends z.ZodTypeAny>(schema: T) =>
+	z.object({
+		success: z.literal(true),
+		data: schema,
+		meta: z.record(z.string(), z.any()).optional(),
+	});
 const ErrorResponse = z.object({
 	success: z.literal(false),
 	error: z.object({
 		code: z.string(),
 		message: z.string(),
-		details: z.any().optional()
-	})
-})
+		details: z.any().optional(),
+	}),
+});
 
-export const createApiResponse = <T extends z.ZodTypeAny>(schema: T) => z.union([SuccessResponse(schema), ErrorResponse])
-export type ApiResponseType<T extends z.ZodTypeAny> = z.infer<ReturnType<typeof createApiResponse<T>>>
+export const createApiResponse = <T extends z.ZodTypeAny>(schema: T) =>
+	z.union([SuccessResponse(schema), ErrorResponse]);
+export type ApiResponseType<T extends z.ZodTypeAny> = z.infer<
+	ReturnType<typeof createApiResponse<T>>
+>;
