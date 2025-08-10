be53f8d22d2b31bde3f14d30a08efdea88b7c920
Winterson <u19cse1028@cit.ac.in>
2025-08-10 23:31:48 +0530

    add: USERS constant, a hard-coded user details container. Add update, create and get all users function

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,13 +1,54 @@
-import type { User } from "@/entities"
+import type { CreateUserDtoType, UpdateUserDtoType } from "@/dtos";
+import type { User } from "@/entities";
 
 // Hardcoded value for simple-testing
+const USERS = [
+	{
+		id: "1",
+		name: "John Doe",
+		email: "johndoe@mail.com",
+		password: "johndoepassword",
+	},
+	{
+		id: "2",
+		name: "Jane Doe",
+		email: "janedoe@mail.com",
+		password: "janedoepassword",
+	},
+	{
+		id: "3",
+		name: "Bob Doe",
+		email: "bobdoe@mail.com",
+		password: "bobdoepassword",
+	},
+	{
+		id: "4",
+		name: "Rob Doe",
+		email: "robdoe@mail.com",
+		password: "robdoepassword",
+	},
+];
+
 export class UserRepository {
 	getUserById(id: string): User | null {
+		const user = USERS.find((_user) => _user.id === id);
+		return user ?? null;
+	}
+	getUsers(): User[] | null {
+		return USERS;
+	}
+	createUser(dto: CreateUserDtoType): User | null {
 		return {
-			id,
-			name: "John Doe",
-			email: "johndoe@mail.com",
-			password: "somethingIsSomethingElse"
-		}
+			id: Math.random().toString(),
+			name: dto.name,
+			email: dto.email,
+			password: dto.password,
+		};
+	}
+	updateUser(dto: UpdateUserDtoType): User | null {
+		const user = USERS.find((_user) => _user.id === dto.id);
+		if (!user) return null;
+		const updatedUser = { ...user, ...dto };
+		return updatedUser;
 	}
 }
