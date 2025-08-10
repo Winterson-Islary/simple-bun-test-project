a3c811e3b4880f7ead3cca2baf36ef4c6137ae35
Winterson <u19cse1028@cit.ac.in>
2025-08-10 23:30:28 +0530

    add: user update and create dtos

--- src/dtos/user.dto.ts
+++ src/dtos/user.dto.ts
@@ -1,9 +1,23 @@
-import z from "zod"
+import z from "zod";
 
 export const UserDto = z.object({
 	id: z.string(),
 	name: z.string(),
 	email: z.email(),
-})
+});
+export type UserDtoType = z.infer<typeof UserDto>;
 
-export type UserDtoType = z.infer<typeof UserDto>
+export const CreateUserDto = z.object({
+	name: z.string(),
+	email: z.email(),
+	password: z.string(),
+});
+export type CreateUserDtoType = z.infer<typeof CreateUserDto>;
+
+export const UpdateUserDto = z.object({
+	id: z.string(),
+	name: z.string().optional(),
+	email: z.email().optional(),
+	password: z.string().optional(),
+});
+export type UpdateUserDtoType = z.infer<typeof UpdateUserDto>;
