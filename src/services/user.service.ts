36974ae2df962dd8087c9ff578d799c8dfd8bcc5
Winterson <u19cse1028@cit.ac.in>
2025-08-10 23:32:12 +0530

    add: get, create and update user service

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,14 +1,30 @@
-import { UserRepository } from "@/repositories"
-import { toUserDto } from "@/mappers"
+import { UserRepository } from "@/repositories";
+import { toUserDto } from "@/mappers";
+import type { CreateUserDtoType, UpdateUserDtoType } from "@/dtos";
 
 export class UserService {
-	private repository: UserRepository
+	private repository: UserRepository;
 	constructor() {
-		this.repository = new UserRepository()
+		this.repository = new UserRepository();
 	}
 	getUser(id: string) {
-		const user = this.repository.getUserById(id)
-		if (!user) return null
-		return toUserDto(user)
+		const user = this.repository.getUserById(id);
+		if (!user) return null;
+		return toUserDto(user);
+	}
+	getUsers() {
+		const users = this.repository.getUsers();
+		if (!users) return null;
+		return users.map((user) => toUserDto(user));
+	}
+	createUser(dto: CreateUserDtoType) {
+		const user = this.repository.createUser(dto);
+		if (!user) return null;
+		return toUserDto(user);
+	}
+	updateUser(dto: UpdateUserDtoType) {
+		const user = this.repository.updateUser(dto);
+		if (!user) return null;
+		return toUserDto(user);
 	}
 }
